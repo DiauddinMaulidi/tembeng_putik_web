b278c028aeff722533aba27c2a6e0bab57c5144b
Diak Uddin Maulidi <d14k.010@gmail.com>
2025-12-15 10:12:53 +0800

    donee

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -5,15 +5,12 @@ const JWT_SECRET = process.env.JWT_SECRET || "secret-key-yang-sangat-rahasia";
 module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    // ❌ Tidak ada header
     if (!authHeader) {
         return res.status(401).json({ message: "Token tidak tersedia" });
     }
 
-    // Format: Bearer TOKEN
     const token = authHeader.split(" ")[1];
 
-    // ❌ Format salah
     if (!token) {
         return res.status(401).json({ message: "Format token salah" });
     }
@@ -21,7 +18,6 @@ module.exports = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
 
-        // Simpan userId ke request
         req.userId = decoded.id;
 
         next();
